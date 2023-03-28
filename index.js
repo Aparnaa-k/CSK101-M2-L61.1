258b8edff91816334f7612951ce2e2bb5c2f324c
Aparnaa-k <119048424+Aparnaa-k@users.noreply.github.com>
2023-03-28 17:25:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const sehwag = document.getElementById('sehwag');
+const sehwagImg = document.getElementById('sehwagimg');
+
+sehwag.onclick = () => {
+  sehwagImg.style.display = 'block';
+};
+
+sehwagImg.onclick = () => {
+  sehwagImg.style.display = 'none';
+};
+
+const sachin = document.getElementById('sachin');
+const sachinImg = document.getElementById('sachinimg');
+
+sachin.onclick = () => {
+  sachinImg.style.display = 'block';
+};
+
+sachinImg.onclick = () => {
+  sachinImg.style.display = 'none';
+};
+
+const gautam = document.getElementById('gautam');
+const gautumImg = document.getElementById('gautumimg');
+
+gautam.onclick = () => {
+  gautumImg.style.display = 'block';
+};
+
+gautumImg.onclick = () => {
+  gautumImg.style.display = 'none';
+};
+
+const virat = document.getElementById('virat');
+const viratImg = document.getElementById('viratimg');
+
+virat.onclick = () => {
+  viratImg.style.display = 'block';
+};
+
+viratImg.onclick = () => {
+  viratImg.style.display = 'none';
+};
+
+const dhoni = document.getElementById('dhoni');
+const dhoniImg = document.getElementById('dhoniimg');
+
+dhoni.onclick = () => {
+  dhoniImg.style.display = 'block';
+};
+
+dhoniImg.onclick = () => {
+  dhoniImg.style.display = 'none';
+};
+
+const yuvaraj = document.getElementById('yuvaraj');
+const yuvaImg = document.getElementById('yuvaimg');
+
+yuvaraj.onclick = () => {
+  yuvaImg.style.display = 'block';
+};
+
+yuvaImg.onclick = () => {
+  yuvaImg.style.display = 'none';
+};
+
+const raina = document.getElementById('raina');
+const sureshImg = document.getElementById('sureshimg');
+
+raina.onclick = () => {
+  sureshImg.style.display = 'block';
+};
+
+sureshImg.onclick = () => {
+  sureshImg.style.display = 'none';
+};
+
+const har = document.getElementById('har');
+const harImg = document.getElementById('harimg');
+
+har.onclick = () => {
+  harImg.style.display = 'block';
+};
+
+harImg.onclick = () => {
+  harImg.style.display = 'none';
+};
+
+const zaheer = document.getElementById('zaheer');
+const zahImg = document.getElementById('zahimg');
+
+zaheer.onclick = () => {
+  zahImg.style.display = 'block';
+};
+
+zahImg.onclick = () => {
+  zahImg.style.display = 'none';
+};
+
+const munaf = document.getElementById('munaf');
+const munafImg = document.getElementById('munafimg');
+
+munaf.onclick = () => {
+  munafImg.style.display = 'block';
+};
+
+munafImg.onclick = () => {
+  munafImg.style.display = 'none';
+};
+
+const sree = document.getElementById('sree');
+const sreeImg = document.getElementById('sreeimg');
+
+sree.onclick = () => {
+  sreeImg.style.display = 'block';
+};
+
+sreeImg.onclick = () => {
+  sreeImg.style.display = 'none';
+};
